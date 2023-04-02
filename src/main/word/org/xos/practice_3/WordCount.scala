d40d1ed1d6e743b2cb06828fb434355aa9b772ad
TOM-XOS <ARBDD_2025y@outlook.com>
2023-04-02 12:49:43 +0800

    V1.0.3

--- src/main/word/org/xos/practice_3/WordCount.scala
+++ src/main/word/org/xos/practice_3/WordCount.scala
@@ -9,5 +9,6 @@ object WordCount {
     val get_data = sc.textFile("./data/word.txt").flatMap(x => x.split(" ")).map(x => (x, 1))
     val w_Count = get_data.reduceByKey((x, y) => x + y).sortBy(x => x._2, ascending = false, numPartitions = 4)
     w_Count.foreach(println)
+    println("成功")
   }
 }
