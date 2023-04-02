5b31ecdbebc355f991040f7848991fa562e6ee3c
TOM-XOS <ARBDD_2025y@outlook.com>
2023-04-02 13:00:09 +0800

    V1.0.6

--- src/main/word/org/xos/practice_3/WordCount.scala
+++ src/main/word/org/xos/practice_3/WordCount.scala
@@ -7,7 +7,7 @@ object WordCount {
     val wordCount = new SparkConf().setMaster("local").setAppName("WordCount")
     val sc = new SparkContext(wordCount)
     val get_data = sc.textFile("./data/word.txt").flatMap(x => x.split(" ")).map(x => (x, 1))
-    val w_Count = get_data.reduceByKey((x, y) => x + y).sortBy(x => x._2, ascending = false, numPartitions = 4)
-    w_Count.foreach(println)
+    val wordSort = get_data.reduceByKey((x, y) => x + y).sortBy(x => x._2, ascending = false, numPartitions = 4)
+    wordSort.foreach(println)
   }
 }
